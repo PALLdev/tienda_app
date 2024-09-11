dfa2af4d8a0170da04680e49fd988b8668097371
PALLdev <pblolara21@gmail.com>
2024-09-11 00:22:38 -0300

    add stack navigator

---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,31 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '../screens/home/HomeScreen';
+import {LoginScreen} from '../screens/auth/LoginScreen';
+import {RegisterScreen} from '../screens/auth/RegisterScreen';
+import {LoadingScreen} from '../screens/loading/LoadingScreen';
+import {ProductScreen} from '../screens/product/ProductScreen';
+
+export type RootStackParams = {
+  Home: undefined;
+  Login: undefined;
+  Register: undefined;
+  Loading: undefined;
+  Product: {productId: string};
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Loading" component={LoadingScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
+    </Stack.Navigator>
+  );
+};
