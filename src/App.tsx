764b51d17baa0a83ecd8bc56c5bc3183dac25967
PALLdev <pblolara21@gmail.com>
2024-09-19 04:08:08 -0300

    add ui-kitten & eva icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './gesture-handler';
 
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {useColorScheme} from 'react-native';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './presentation/navigation/StackNavigator';
 
 export const App = () => {
+  const colorScheme = useColorScheme();
+  const themeMode = colorScheme === 'dark' ? eva.dark : eva.light;
+
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={themeMode}>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,16 @@
-import {Text, View} from 'react-native';
+import {Button, Icon, Layout, Text} from '@ui-kitten/components';
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text category="h4">HOME SCREEN</Text>
+      <LoginButton />
+    </Layout>
   );
 };
+
+const FacebookIcon = (props: any) => <Icon name="facebook" {...props} />;
+
+export const LoginButton = () => (
+  <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
+);
